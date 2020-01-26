6a8701b3afae02ed34baff88122b68c3a0b2ae82
mastaBlasta <superad228@gmail.com>
2020-01-27 00:01:31 +0300

    changes for deployment

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import git
+
+from flask import Flask, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -15,6 +17,17 @@ mysql = MySQL(app)
 @app.route('/', methods=['GET'])
 def home():
     return '', 404
+
+
+@app.route('/update_back', methods=['POST'])
+def mai_back_update():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/MastaBlastaIT/mai_back.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 import flask_app.set_table
