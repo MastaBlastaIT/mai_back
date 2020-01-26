e30cabc6b5d8d4797ecdc5531f464c66b8a11c9e
mastaBlasta <superad228@gmail.com>
2020-01-26 22:09:20 +0300

    change __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,17 +3,11 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# MySQL localhost configurations
-app.config['MYSQL_USER'] = 'regexp_user'
-app.config['MYSQL_PASSWORD'] = 'fRXsW7A9'
-app.config['MYSQL_DB'] = 'regexp_texts'
-app.config['MYSQL_HOST'] = 'localhost'
-
 # MySQL pythonanywhere configurations
-# app.config['MYSQL_USER'] = 'mastaBlastaIT'
-# app.config['MYSQL_PASSWORD'] = '9@RYKQTWHtw38qP'
-# app.config['MYSQL_DB'] = 'mastaBlastaIT$regexp_texts'
-# app.config['MYSQL_HOST'] = 'mastaBlastaIT.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'mastaBlastaIT'
+app.config['MYSQL_PASSWORD'] = '9@RYKQTWHtw38qP'
+app.config['MYSQL_DB'] = 'mastaBlastaIT$regexp_texts'
+app.config['MYSQL_HOST'] = 'mastaBlastaIT.mysql.pythonanywhere-services.com'
 
 mysql = MySQL(app)
 
