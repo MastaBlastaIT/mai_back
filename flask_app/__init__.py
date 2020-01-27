38beba4d323b18dedb9d2ba62f14156968a6403e
mastaBlasta <superad228@gmail.com>
2020-01-27 03:16:50 +0300

    secure update fix

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,4 @@
-import git
+import os, git, hmac, hashlib
 
 from flask import Flask, request
 from flask_mysqldb import MySQL
@@ -16,16 +16,33 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    return '', 404
+    return 'test', 200
+
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 
 @app.route('/update_back', methods=['POST'])
 def mai_back_update():
     if request.method == 'POST':
-        repo = git.Repo('https://github.com/MastaBlastaIT/mai_back.git')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        secret_key = os.getenv('SECRET_KEY')
+        check_valid = is_valid_signature(x_hub_signature, request.data, secret_key)
+        if check_valid:
+            repo = git.Repo('/home/mastaBlastaIT/mai_back/')
+            origin = repo.remotes.origin
+            origin.fetch()
+            repo.merge_base(repo.heads.master, origin.refs.master)
+            return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Invalid signature', 401
     else:
         return 'Wrong event type', 400
 
